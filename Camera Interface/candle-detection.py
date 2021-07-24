6d8d21dfd384d17bb42a3c2a521e2786da61f0a4
arjun-krishna1 <arjunkrishna1306@gmail.com>
2021-07-24 19:43:09 -0400

    picture getter

--- Camera Interface/candle-detection.py
+++ Camera Interface/candle-detection.py
@@ -4,25 +4,31 @@ cap = cv2.VideoCapture('output.avi')
 
 frame_width, frame_height = int(cap.get(3)), int(cap.get(4))
 
+counter = 0
 while cap.isOpened():
-	ret, frame = cap.read()
-	if not ret:
-		break
+    ret, frame = cap.read()
+    if not ret:
+        break
 
-	blur = cv2.GaussianBlur(frame, (9, 9), cv2.BORDER_DEFAULT)
-	_, thresh = cv2.threshold(blur, 200, 255, cv2.THRESH_BINARY)
-	dilated = cv2.dilate(thresh, None, iterations = 5)
-	contours, hiers = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-	if contours != []:
-		for contour, hier in zip(contours, hiers[0]):
-			(x, y, w, h) = cv2.boundingRect(contour)
-			cv2.rectangle(previous, (x, y), (x + w, y + h), (0, 255, 0), 2)
-			cv2.putText(previous, "Status: Candle", (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
-						1, (0, 0, 255), 3)
+    blur = cv2.GaussianBlur(frame, (9, 9), cv2.BORDER_DEFAULT)
+    _, thresh = cv2.threshold(blur, 200, 255, cv2.THRESH_BINARY)
+    dilated = cv2.dilate(thresh, None, iterations = 5)
+    img = cv2.cvtColor(dilated, cv2.COLOR_BGR2GRAY)
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
+    if contours != []:
+        for contour, hier in zip(contours, hierarchy[0]):
+            counter += 1
+            (x, y, w, h) = cv2.boundingRect(contour)
+            cv2.rectangle(dilated, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            
+    STATUS = cv2.imwrite(str(counter) + ".png", dilated)
                     
                 
-	cv2.imshow("Camera thing", dilated)
-	if cv2.waitKey(60) == ord("`"):
-		break
+    cv2.imshow("Camera thing", frame)
+    if cv2.waitKey(60) == ord("`"):
+        break
+    
+    
 
 cv2.destroyAllWindows()
