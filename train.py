69c4b918ed953f040db61d2b20f3ac63674bc714
arjunkrishna <arjun.krishna@huawei.com>
2021-07-24 21:42:57 -0400

    add deep learning hotfix

---
+++ train.py
@@ -0,0 +1,64 @@
+import numpy as np
+import os
+import PIL
+import PIL.Image
+import tensorflow as tf
+
+import pathlib
+
+def train():
+	data_dir = pathlib.Path("camera_interface/img/")
+
+	batch_size = 32
+	img_height = 180
+	img_width = 180
+
+	train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+	  data_dir,
+	  validation_split=0.2,
+	  subset="training",
+	  seed=123,
+	  image_size=(img_height, img_width),
+	  batch_size=batch_size)
+	  
+	val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+	  data_dir,
+	  validation_split=0.2,
+	  subset="validation",
+	  seed=123,
+	  image_size=(img_height, img_width),
+	  batch_size=batch_size)
+
+	AUTOTUNE = tf.data.AUTOTUNE
+
+	train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+	val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+	num_classes = 2
+
+	model = tf.keras.Sequential([
+	  tf.keras.layers.experimental.preprocessing.Rescaling(1./255),
+	  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+	  tf.keras.layers.MaxPooling2D(),
+	  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+	  tf.keras.layers.MaxPooling2D(),
+	  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+	  tf.keras.layers.MaxPooling2D(),
+	  tf.keras.layers.Flatten(),
+	  tf.keras.layers.Dense(128, activation='relu'),
+	  tf.keras.layers.Dense(num_classes)
+	])
+
+	model.compile(
+	  optimizer='adam',
+	  loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+	  metrics=['accuracy'])
+
+	model.fit(
+	  train_ds,
+	  validation_data=val_ds,
+	  epochs=3
+	)
+
+	return model
+
